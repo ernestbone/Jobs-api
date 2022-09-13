d21a44827ec2f2ebfc1fedf046624962a83fc35c
ernestbone <owhiroroernest@gmail.com>
2022-09-13 13:43:56 +0100

    Swagger doc added

--- app.js
+++ app.js
@@ -19,21 +19,30 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// extra packages
+//extra packages
+app.use(express.json());
+
+// extra security packages
 app.set('trust proxy', 1);
 app.use(rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 }))
-app.use(express.json());
 app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+//Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 
 app.get('/', (req, res)=>{
-  res.send('jobs api')
+  res.send('<h1>Jobs api</h1><a href="/api-docs">Documentation</a>')
 })
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/Jobs', authenticateUser, jobsRouter)
